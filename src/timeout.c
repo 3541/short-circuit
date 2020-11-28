19ebb8a83e44cb5ef2ee83d7bc7dd5c54ada6ab7
3541 <alex@emobrien.com>
2020-11-28 12:49:42 +1100

    More timeout work.

--- src/timeout.c
+++ src/timeout.c
@@ -9,3 +9,22 @@ INLINE ssize_t timeout_compare(Timeout* lhs, Timeout* rhs) {
 }
 
 LPQ_IMPL_METHODS(Timeout, timeout_compare);
+
+TimeoutQueue timeout_init() {
+    TimeoutQueue ret;
+    LPQ_INIT(Timeout)(&ret.queue);
+    return ret;
+}
+
+bool timeout_schedule(TimeoutQueue* this, Timeout* timeout,
+                      struct io_uring* uring) {
+    assert(this);
+    assert(timeout);
+    assert(!timeout->_lpq_ptr.next && !timeout->_lpq_ptr.prev);
+
+    LPQ_ENQUEUE(Timeout)(&this->queue, timeout);
+    if (LPQ_PEEK(Timeout)(&this->queue) == timeout)
+        return event_timeout_submit(&this->event, uring, timeout->threshold, 0,
+                                    IORING_TIMEOUT_ABS);
+    return true;
+}
--- src/timeout.h
+++ src/timeout.h
@@ -8,9 +8,21 @@
 struct Timeout;
 typedef struct Timeout Timeout;
 
+typedef void (*TimeoutExec)(Connection*, struct io_uring*);
+
 LPQ_IMPL_STRUCTS(Timeout);
 
 struct Timeout {
     time_t      threshold;
+    TimeoutExec time_out;
     LPQ_NODE(Timeout);
 };
+
+typedef struct TimeoutQueue {
+    Event event;
+    LPQ(Timeout) queue;
+} TimeoutQueue;
+
+TimeoutQueue timeout_init(void);
+bool timeout_schedule(TimeoutQueue*, Timeout*, struct io_uring*);
+bool timeout_handle(TimeoutQueue*, struct io_uring*, struct io_uring_cqe*);
