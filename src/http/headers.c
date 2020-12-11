e06556c9276685f901a546f77b86d0d3a918fb81
3541 <alex@emobrien.com>
2020-12-11 16:39:48 +1100

    Preliminary hash table work.

---
+++ src/http/headers.c
@@ -0,0 +1,10 @@
+/*
+ * HEADERS -- HTTP Headers
+ *
+ * Copyright (c) 2020, Alex O'Brien <3541ax@gmail.com>
+ *
+ * This file is licensed under the BSD 3-clause license. See the LICENSE file in
+ * the project root for details.
+ */
+
+#include "http/headers.h"
