054f7447bf214db137d8521ba6dd610381c7c8c3
3541 <alex@emobrien.com>
2021-01-02 18:08:40 +1100

    Add copyright headers to source files.

--- src/http/headers.c
+++ src/http/headers.c
@@ -1,10 +1,20 @@
 /*
- * HEADERS -- HTTP Headers
+ * SHORT CIRCUIT: HTTP HEADERS -- HTTP header parsing and storage.
  *
- * Copyright (c) 2020, Alex O'Brien <3541ax@gmail.com>
+ * Copyright (c) 2021, Alex O'Brien <3541ax@gmail.com>
  *
- * This file is licensed under the BSD 3-clause license. See the LICENSE file in
- * the project root for details.
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include "http/headers.h"
--- src/main.c
+++ src/main.c
@@ -1,3 +1,22 @@
+/*
+ * SHORT CIRCUIT -- A high-performance HTTP server for Linux, built on io_uring.
+ *
+ * Copyright (c) 2020-2021, Alex O'Brien <3541ax@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <errno.h>
 #include <liburing.h>
 #include <liburing/io_uring.h>
