456bed2d8806c92f2ccc47113d0ad52b0f33b8c2
3541 <alex@emobrien.com>
2021-01-12 21:02:35 +1100

    TIDY: Formatting.

--- ssl_test/gtls_test.c
+++ ssl_test/gtls_test.c
@@ -15,15 +15,16 @@ int main(void) {
     }
 
     const int enable = 1;
-    if (setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(enable)) < 0) {
+    if (setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, &enable,
+                   sizeof(enable)) < 0) {
         perror("setsockopt(SO_REUSEADDR)");
         return -1;
     }
 
     struct sockaddr_in addr;
     memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(4433);
+    addr.sin_family      = AF_INET;
+    addr.sin_port        = htons(4433);
     addr.sin_addr.s_addr = INADDR_ANY;
 
     if (bind(listen_sock, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
@@ -35,19 +36,20 @@ int main(void) {
         return -1;
     }
 
-    int rc = -1;
+    int                                   rc = -1;
     gnutls_certificate_server_credentials cred;
     if ((rc = gnutls_certificate_allocate_credentials(&cred)) < 0) {
         fprintf(stderr, "allocate_credentials: %s\n", gnutls_strerror(rc));
         return -1;
     }
-    if ((rc = gnutls_certificate_set_x509_key_file(cred, "cert.pem", "key.pem", GNUTLS_X509_FMT_PEM)) < 0) {
+    if ((rc = gnutls_certificate_set_x509_key_file(cred, "cert.pem", "key.pem",
+                                                   GNUTLS_X509_FMT_PEM)) < 0) {
         fprintf(stderr, "set_x509_key_file: %s\n", gnutls_strerror(rc));
         return -1;
     }
 
     struct sockaddr_in peer;
-    socklen_t peer_len;
+    socklen_t          peer_len;
     int sock = accept(listen_sock, (struct sockaddr*)&peer, &peer_len);
     if (sock < 0) {
         perror("accept");
@@ -70,8 +72,8 @@ int main(void) {
 
     printf("Handshake done.\n");
 
-    char buf[128] = { '\0' };
-    ssize_t count = -1;
+    char    buf[128] = { '\0' };
+    ssize_t count    = -1;
     if ((count = gnutls_record_recv(session, buf, sizeof(buf))) < 0) {
         fprintf(stderr, "record_recv: %s\n", gnutls_strerror((int)count));
         return -1;
