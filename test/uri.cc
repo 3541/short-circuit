8eb9f0cb897b2dcf2c33280f9d5c95d5d0148a8c
3541 <3541ax@gmail.com>
2021-01-24 15:47:31 +1100

    URI+TEST: Add another URI test.

--- test/uri.cc
+++ test/uri.cc
@@ -24,3 +24,28 @@ TEST_F(UriTest, parse_trivial) {
 
     a3_string_free(&s);
 }
+
+TEST_F(UriTest, parse_scheme_authority) {
+    A3String s1 = a3_string_clone(A3_CS("http://example.com/test.txt"));
+    A3String s2 = a3_string_clone(A3_CS("https://example.com/asdf.txt"));
+
+    EXPECT_EQ(uri_parse(&uri, s1), URI_PARSE_SUCCESS);
+    EXPECT_EQ(uri.scheme, URI_SCHEME_HTTP);
+    EXPECT_EQ(a3_string_cmp(A3_S_CONST(uri.authority), A3_CS("example.com")),
+              0);
+    EXPECT_EQ(a3_string_cmp(A3_S_CONST(uri.path), A3_CS("/test.txt")), 0);
+    EXPECT_FALSE(uri.query.ptr);
+    EXPECT_FALSE(uri.fragment.ptr);
+    uri_free(&uri);
+
+    EXPECT_EQ(uri_parse(&uri, s2), URI_PARSE_SUCCESS);
+    EXPECT_EQ(uri.scheme, URI_SCHEME_HTTPS);
+    EXPECT_EQ(a3_string_cmp(A3_S_CONST(uri.authority), A3_CS("example.com")),
+              0);
+    EXPECT_EQ(a3_string_cmp(A3_S_CONST(uri.path), A3_CS("/asdf.txt")), 0);
+    EXPECT_FALSE(uri.query.ptr);
+    EXPECT_FALSE(uri.fragment.ptr);
+
+    a3_string_free(&s1);
+    a3_string_free(&s2);
+}
