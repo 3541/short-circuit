0f7b62ea4925047ecc482500be8e488930359263
Alex O'Brien <3541ax@gmail.com>
2021-05-30 16:02:46 +1000

    EVENT: Move event initialization into its own file.

--- src/event/mod.cc
+++ src/event/mod.cc
@@ -24,12 +24,9 @@
 #include <cstdlib>
 #include <fcntl.h>
 #include <liburing.h>
-#include <liburing/io_uring.h>
 #include <memory>
 #include <string.h>
-#include <sys/resource.h>
 #include <sys/types.h>
-#include <sys/utsname.h>
 #include <unistd.h>
 #include <utility>
 
@@ -46,117 +43,16 @@
 using std::move;
 using std::unique_ptr;
 
+using ExpectedStatus = Event::ExpectedStatus;
+
 A3_POOL_STORAGE(Event, EVENT_POOL_SIZE, A3_POOL_ZERO_BLOCKS, nullptr);
 A3_SLL_DEFINE_METHODS(Event);
 
-Event::Event(EventTarget* tgt, EventType ty, int32_t expected_return, bool chain, uint32_t fl,
-             bool force_handle, bool queue) :
-    type { ty }, status { expected_return }, target { tgt } {
-    flags = ((chain && !force_handle) ? EVENT_FLAG_CHAIN : 0) | fl;
+Event::Event(EventTarget* tgt, EventCallback cb, void* cb_data, int32_t expected_return,
+             bool queue) :
+    status { expected_return }, target { tgt }, callback { cb }, callback_data { cb_data } {
     if (queue)
         A3_SLL_PUSH(Event)(tgt, this);
-}
-
-A3CString event_type_name(EventType ty) {
-#define _EVENT_TYPE(E) [E]               = A3_CS(#E),
-    static const A3CString EVENT_NAMES[] = { EVENT_TYPE_ENUM };
-#undef _EVENT_TYPE
-
-    if (!(0 <= ty && static_cast<size_t>(ty) < sizeof(EVENT_NAMES) / sizeof(EVENT_NAMES[0])))
-        return A3_CS("INVALID");
-
-    return EVENT_NAMES[ty];
-}
-
-// Check that the kernel is recent enough to support io_uring and
-// io_uring_probe.
-static void event_check_kver(void) {
-    struct utsname info;
-    A3_UNWRAPSD(uname(&info));
-
-    char* release = strdup(info.release);
-
-    long version_major = strtol(strtok(info.release, "."), nullptr, 10);
-    long version_minor = strtol(strtok(nullptr, "."), nullptr, 10);
-
-    if (version_major < MIN_KERNEL_VERSION_MAJOR ||
-        (version_major == MIN_KERNEL_VERSION_MAJOR && version_minor < MIN_KERNEL_VERSION_MINOR))
-        A3_PANIC_FMT("Kernel version %s is not supported. At least %d.%d is required.", release,
-                     MIN_KERNEL_VERSION_MAJOR, MIN_KERNEL_VERSION_MINOR);
-
-    free(release);
-}
-
-#define REQUIRE_OP(P, OP)                                                                          \
-    do {                                                                                           \
-        if (!io_uring_opcode_supported(P, OP))                                                     \
-            A3_PANIC_FMT("Required io_uring op %s is not supported by the kernel.", #OP);          \
-    } while (0)
-
-// All ops used should be checked here.
-static void event_check_ops(struct io_uring* uring) {
-    struct io_uring_probe* probe = io_uring_get_probe_ring(uring);
-
-    REQUIRE_OP(probe, IORING_OP_ACCEPT);
-    REQUIRE_OP(probe, IORING_OP_ASYNC_CANCEL);
-    REQUIRE_OP(probe, IORING_OP_CLOSE);
-    REQUIRE_OP(probe, IORING_OP_READ);
-    REQUIRE_OP(probe, IORING_OP_RECV);
-    REQUIRE_OP(probe, IORING_OP_SEND);
-    REQUIRE_OP(probe, IORING_OP_SPLICE);
-    REQUIRE_OP(probe, IORING_OP_TIMEOUT);
-
-    free(probe);
-}
-
-// Set the given resource to its hard limit and return the new state.
-static struct rlimit rlimit_maximize(int resource) {
-    struct rlimit lim;
-
-    A3_UNWRAPSD(getrlimit(resource, &lim));
-    lim.rlim_cur = lim.rlim_max;
-    A3_UNWRAPSD(setrlimit(resource, &lim));
-    return lim;
-}
-
-static void event_check_rlimits(void) {
-    struct rlimit lim_memlock = rlimit_maximize(RLIMIT_MEMLOCK);
-
-    // This is a crude check, but opening the queue will almost certainly fail
-    // if the limit is this low.
-    if (lim_memlock.rlim_cur <= 96 * URING_ENTRIES)
-        a3_log_fmt(LOG_WARN,
-                   "The memlock limit (%d) is too low. The queue will probably "
-                   "fail to open. Either raise the limit or lower `URING_ENTRIES`.",
-                   lim_memlock.rlim_cur);
-
-    struct rlimit lim_nofile = rlimit_maximize(RLIMIT_NOFILE);
-    if (lim_nofile.rlim_cur <= CONNECTION_POOL_SIZE * 3)
-        a3_log_fmt(LOG_WARN,
-                   "The open file limit (%d) is low. Large numbers of concurrent "
-                   "connections will probably cause \"too many open files\" errors.",
-                   lim_nofile.rlim_cur);
-}
-
-struct io_uring event_init() {
-    event_check_kver();
-    event_check_rlimits();
-
-    struct io_uring ret;
-
-    bool opened = false;
-    for (size_t queue_size = URING_ENTRIES; queue_size >= 512; queue_size /= 2) {
-        if (!io_uring_queue_init(URING_ENTRIES, &ret, 0)) {
-            opened = true;
-            break;
-        }
-    }
-    if (!opened)
-        A3_PANIC("Unable to open queue. The memlock limit is probably too low.");
-
-    event_check_ops(&ret);
-
-    return ret;
 }
 
 // It is expected that there should not be a situation which causes an sqe_ptr to be destroyed. If
