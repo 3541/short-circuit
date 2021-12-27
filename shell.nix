cfd70cc4c9727309ec8a9d8da2c8afe33b118b61
Alex O'Brien <3541ax@gmail.com>
2021-12-27 15:44:58 +1100

    TIDY: Nix.

---
+++ shell.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell {
+  packages = with pkgs; [
+    gcc
+    gdb
+    rr
+    # NOTE: The placement of clang-tools /before/ llvmPackages.clang is critical. Otherwise, the
+    # latter package will provide an un-wrapped `clangd`, which is unable to find system headers.
+    (clang-tools.override { llvmPackages = llvmPackages_12; })
+    llvmPackages_latest.clang
+    doxygen
+    meson
+    gtest
+    pkg-config
+    ninja
+    liburing
+  ];
+}
