e925a21d225cc19253f74471ab6fe21aeb348b3a
Alex O'Brien <3541ax@gmail.com>
2022-03-31 13:22:05 +1100

    IO: Only fall back to readv on old kernels.

--- src/io/backend/uring.c
+++ src/io/backend/uring.c
@@ -19,6 +19,8 @@
 
 #include <assert.h>
 #include <liburing.h>
+#include <limits.h>
+#include <linux/version.h>
 #include <sys/resource.h>
 #include <sys/utsname.h>
 
@@ -241,12 +243,17 @@ ssize_t sc_io_read(ScCoroutine* self, ScFd fd, A3String dst, off_t offset) {
     assert(self);
     assert(fd >= 0);
     assert(dst.ptr);
+    assert(dst.len <= UINT_MAX);
 
     struct io_uring_sqe* sqe = sc_io_sqe_get(self);
     A3_TRYB_MAP(sqe, SC_IO_SUBMIT_FAILED);
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 6, 0)
+    io_uring_prep_read(sqe, fd, dst.ptr, (unsigned int)dst.len, (uint64_t)offset);
+#else
     struct iovec vec[] = { { .iov_base = dst.ptr, .iov_len = dst.len } };
     io_uring_prep_readv(sqe, fd, vec, 1, (uint64_t)offset);
+#endif
 
     return sc_io_submit(self, sqe);
 }
