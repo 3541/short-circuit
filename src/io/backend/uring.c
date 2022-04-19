8e85bab958928e4df966d8d5420ac8444709f2a2
Alex O'Brien <3541ax@gmail.com>
2022-04-19 17:19:44 +1000

    IO(URING): Handle short read in read.

--- src/io/backend/uring.c
+++ src/io/backend/uring.c
@@ -286,21 +286,32 @@ sc_io_read(ScCoroutine* self, ScFd fd, A3String dst, size_t count, off_t offset)
     assert(dst.ptr);
     assert(dst.len <= UINT_MAX);
 
-    struct io_uring_sqe* sqe = sc_io_sqe_get(self);
-    A3_TRYB_MAP(sqe, SC_IO_ERR(size_t, SC_IO_SUBMIT_FAILED));
-
     size_t to_read = MIN(count, dst.len);
+    size_t left    = to_read;
+
+    while (left > 0) {
+        struct io_uring_sqe* sqe = sc_io_sqe_get(self);
+        A3_TRYB_MAP(sqe, SC_IO_ERR(size_t, SC_IO_SUBMIT_FAILED));
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 6, 0)
-    io_uring_prep_read(sqe, fd, dst.ptr, (unsigned int)to_read, (uint64_t)offset);
+        io_uring_prep_read(sqe, fd, dst.ptr, (unsigned int)to_read, (uint64_t)offset);
 #else
-    struct iovec vec[] = { { .iov_base = dst.ptr, .iov_len = to_read } };
-    io_uring_prep_readv(sqe, fd, vec, 1, (uint64_t)offset);
+        struct iovec vec[] = { { .iov_base = dst.ptr, .iov_len = to_read } };
+        io_uring_prep_readv(sqe, fd, vec, 1, (uint64_t)offset);
 #endif
 
-    ssize_t res = -1;
-    A3_UNWRAPS(res, sc_io_submit(self, sqe));
-    return SC_IO_OK(size_t, (size_t)res);
+        ssize_t res = -1;
+        A3_UNWRAPS(res, sc_io_submit(self, sqe));
+
+        left -= (size_t)res;
+        offset += res;
+        dst = a3_string_offset(dst, (size_t)res);
+
+        if (!res)
+            break;
+    }
+
+    return SC_IO_OK(size_t, to_read - left);
 }
 
 SC_IO_RESULT(size_t)
